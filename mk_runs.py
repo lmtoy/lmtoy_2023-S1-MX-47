3685008b13e291401f87c733543e021ca3d9eef4
Peter Teuben <teuben@gmail.com>
2023-02-06 09:55:34 -0500

    new

---
+++ mk_runs.py
@@ -0,0 +1,38 @@
+#! /usr/bin/env python
+#
+#   script generator for project="2021-S1-US-3"
+#
+#   lmtinfo.py grep US-3 Science Map | awk '{print $2}' | sort
+
+
+import os
+import sys
+
+# in prep of the new lmtoy module
+try:
+    lmtoy = os.environ['LMTOY']
+    sys.path.append(lmtoy)
+    from lmtoy import runs
+except:
+    print("No LMTOY with runs.py")
+    sys.exit(0)
+
+project="2023-S1-MX-47"
+
+#        obsnums per source (make it negative if not added to the final combination)
+on = {}
+on["9487-9102"] = [ 104790, 104791, 104792, 104794, 104795, 104796, 104798, 104799,
+                    104800, 104803, 104804, 104805, 104807, 104808, 104809, 104811,
+                    104812, 104813, 104828, 104829, 104830, 104832, 104833]           # feb 6
+
+
+#        common parameters per source on the first dryrun (run1a, run2a)
+pars1 = {}
+pars1["9487-9102"] = ""
+
+
+#        common parameters per source on subsequent runs (run1b, run2b)
+pars2 = {}
+pars2["9487-9102"] = "srdp=1 admit=0"
+
+runs.mk_runs(project, on, pars1, pars2)
